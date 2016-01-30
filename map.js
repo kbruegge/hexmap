49152db21373caffa68aa5e91222eeefe25dfd98
Kai <kai.bruegge@gmail.com>
2016-01-30 16:24:19 +0100

    trying to make this more readable. JS is weird.

--- map.js
+++ map.js
@@ -1,209 +1,143 @@
+function hex() {
+  function log(message){
+      $('#debug').append('<p>' + message + '</p>');
+  }
 
 
-function main() {
-    $('#softidbutton').click( function(){
-        showsoftids();
-    });
-    $('#chidbutton').click( function(){
-        showchids();
-    });
-    $('#colorbutton').click( function(){
-        showcolors();
-    });
-    $('#geombutton').click( function(){
-        showgeomids();
-    });
-    
-function log(message){
-    $('#debug').append('<p>' + message + '</p>');
-}
+  //get three variables, margin, width, height. width and height will be the dimensions of the svg container
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 960 - margin.top - margin.bottom;
 
-var points = [];
-var radius = 10,
-    offset = 0;
-
-//-------init the coordinate system and the svg thingy
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 960 - margin.top - margin.bottom;
-
-
-var color = d3.scale.linear()
-    .domain([0, 20])
-    .range(["white", "white"])
-    .interpolate(d3.interpolateLab);
-
-var x = d3.scale.identity()
-    .domain([0, width]);
-
-var y = d3.scale.linear()
-    .domain([0, height])
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickSize(6, -height);
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickSize(6, -width);
-
-var svg = d3.select("div#container").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-
-//------------ load the csv and call the drawsvg function on success
-d3.csv("pixel-map-with-chids.csv")
-    .row(function (d) { points.push(d); })
+var svg = d3.select("body")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height);
+var radius = 10;
+var offset_x  = 0;
+var offset_y  = 0;
+d3.csv("./pixel_positions.csv")
+    .row(function (csv_row) {
+        // console.log(csv_row);
+        // center = pixelCenter(csv_row, radius=10);
+        //now paint stuff in here
+        // console.log(center);
+        // points.append(csv_row);
+        offset_x = Math.max(offset_x, csv_row.pos_X);
+        offset_y = Math.max(offset_y, csv_row.pos_Y);
+        return csv_row;
+    })
     .get(function(error, rows) {
-        console.log(error);
-        //console.log(points);
-        drawSVG();
-    });
+        // console.log(error);
+        // console.log(rows);
+
+        var circles = svg.selectAll("circle")
+                              .data(rows)
+                            .enter()
+                              .append("svg:circle")
+                              .attr("cx", function (d, i){
+                                return d.pos_X*2*radius + offset_x;
+                              })
+                              .attr("cy",  function (d, i){
+                                return d.pos_Y*2*radius + offset_y;
+                              });
+
+        var circleAttributes = circles
+                                      .attr("r", radius)
+                                      .style("fill", function (d) {return "#858587";});
+});
 
 
-    
-//-------------
-//   These functions get a row from the csv and return some stuff based on that data
-// 
-//    !!! X AND Y COORDINATES FROM  THE FACT-MAP ARE SWITCHED!!!!
-//-------
-var circleCenterX = function (d) {
-        return -(d.pos_Y) * 2 * (radius + offset) +  width / 2; 
-    };
 
-var textCenterX = function (d){
-    return circleCenterX(d) - (radius* 0.8); 
-    };
-var textCenterY = function (d){
-    return circleCenterY(d) + (radius* 0.3); 
-    };
 
-var circleCenterY = function (d){
-    return -(d.pos_X)*2*(radius + offset) +  width / 2; 
-};
 
-var softIDtext = function (d) {
-        return d.softID;
-};
-var chIDtext = function (d) {
-        return d.CHID;
-};
-var geomIDtext = function (d) {
-        return d.geom_i + ", " + d.geom_j;
-};
-    
-    
-//_-------------
-  
-var drawSVG = function () {
-    var circles = svg.selectAll("circle")
-                          .data(points)
-                          .enter()
-                          .append("circle");
+  var pixelCenter = function(d, radius){
+      x = -d.pos_Y * radius;
+      y = -d.pos_X * radius;
+      return {id : d.id,  x:x, y:y };
+  };
 
-    var circleAttributes = circles
-                       .attr("cx", circleCenterX)
-                       .attr("cy", circleCenterY)
-                       .attr("r", radius)
-                    .style("fill", function (d) {return "lightgrey";});
-    
-    showchids();
-    
-};
+  // var color = d3.scale.linear()
+  //     .domain([0, 20])
+  //     .range(["white", "white"])
+  //     .interpolate(d3.interpolateLab);
+  //
+  // var x = d3.scale.identity()
+  //     .domain([0, width]);
+  //
+  // var y = d3.scale.linear()
+  //     .domain([0, height])
+  //     .range([height, 0]);
+  //
+  // var xAxis = d3.svg.axis()
+  //     .scale(x)
+  //     .orient("bottom")
+  //     .tickSize(6, -height);
+  //
+  // var yAxis = d3.svg.axis()
+  //     .scale(y)
+  //     .orient("left")
+  //     .tickSize(6, -width);
+  //
+  // var svg = d3.select("div#container").append("svg")
+  //     .attr("width", width + margin.left + margin.right)
+  //     .attr("height", height + margin.top + margin.bottom)
+  //     .append("g")
+  //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  //
+  // svg.append("g")
+  //     .attr("class", "y axis")
+  //     .call(yAxis);
+  //
+  // svg.append("g")
+  //     .attr("class", "x axis")
+  //     .attr("transform", "translate(0," + height + ")")
+  //     .call(xAxis);
 
-var colorFromId= function (number){
-    //var normalized = number/255.0
-    var color = tinycolor("#ea4040");
-    color = tinycolor.spin(color, number/1440.0 * 255.0)
-    //color = tinycolor.saturate(color, number/1440.0 * 255.0)
-    //log(color.toHexString());
-    //console.log(color.toHexString());
-    return color.toHexString();           
+
+  //------------ load the csv and call the drawsvg function on success
+
+  //
+  //
+  //
+  //
+  //
+  // var colorFromId= function (number){
+  //     //var normalized = number/255.0
+  //     var color = tinycolor("#ea4040");
+  //     color = tinycolor.spin(color, number/1440.0 * 255.0);
+  //     //color = tinycolor.saturate(color, number/1440.0 * 255.0)
+  //     //log(color.toHexString());
+  //     //console.log(color.toHexString());
+  //     return color.toHexString();
+  // };
+  //
+  // function showcolors(){
+  //     var circles = svg.selectAll("circle");
+  //     var circleAttributes = circles.style("fill", function (d) {return colorFromId(d.id);});
+  // }
+  //
+  // function showGreyScale(){
+  //     var circles = svg.selectAll("circle");
+  //     var circleAttributes = circles.style("fill", function (d) {return colorFromId(d.id);});
+  // }
+  // function showsoftids(){
+  //     //console.log(d3.selectAll("#textid"));
+  //
+  //     d3.selectAll("#textid").remove();
+  //
+  //     var texts = svg.selectAll("textIds")
+  //                 .data(points);
+  //
+  //     texts.enter().append("text")
+  //         .text(softIDtext);
+  //
+  //     texts.attr("x", textCenterX)
+  //         .attr("y", textCenterY)
+  //         .attr("font-size", 7)
+  //         .attr("font-family", "sans-serif")
+  //         .attr("id","textid");
+  // }
+
 }
-//------- update the drawn text
-
-function showgeomids(){
-    d3.selectAll("#textid").remove();
-    var texts = svg.selectAll("textIds")
-                .data(points);
-    log("showing geom ids")
-    texts.enter().append("text")
-        .text(geomIDtext);
-    
-    texts.attr("x", textCenterX)
-        .attr("y", textCenterY)
-        .attr("font-size", 7)
-        .attr("font-family", "sans-serif")
-        .attr("id","textid");    
-}
-function showchids(){
-    d3.selectAll("#textid").remove();
-    var texts = svg.selectAll("textIds")
-                .data(points);
-                
-    texts.enter().append("text")
-        .text(chIDtext);
-    
-    texts.attr("x", textCenterX)
-        .attr("y", textCenterY)
-        .attr("font-size", 7)
-        .attr("font-family", "sans-serif")
-        .attr("id","textid");
-}
-    
-function showcolors(){
-    //log("button pressed")
-    var circles = svg.selectAll("circle");
-    var circleAttributes = circles.style("fill", function (d) {return colorFromId(d.CHID);});  
-}
-
-function showGreyScale(){
-    var circles = svg.selectAll("circle");
-    var circleAttributes = circles.style("fill", function (d) {return colorFromId(d.softID);});
-}
-function showsoftids(){
-    //console.log(d3.selectAll("#textid"));
-    
-    d3.selectAll("#textid").remove();
-    
-    var texts = svg.selectAll("textIds")
-                .data(points);
-                
-    texts.enter().append("text")
-        .text(softIDtext);
-    
-    texts.attr("x", textCenterX)
-        .attr("y", textCenterY)
-        .attr("font-size", 7)
-        .attr("font-family", "sans-serif")
-        .attr("id","textid");
-    /*        
-    texts.enter().append("text")
-        .text(softIDtext);
-    texts.exit().remove();
-    
-    texts.attr("x", textCenterX)
-        .attr("y", textCenterY)
-        .attr("font-size", 7)
-        .attr("font-family", "sans-serif")
-        .attr("id","textid");
-      */          
-}
-    
-    
-};
+// main();
