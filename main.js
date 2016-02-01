7292f0ca38ced4723a851dc49053f4d864963d6c
Kai <kai.bruegge@gmail.com>
2016-02-01 08:19:22 +0100

    Make the hexmap an object

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const $ = require('zeptojs');
 const map = require('./map.js');
+const d3 = require('d3');
 
 $(document).ready(init);
 
 function init() {
-  var name = 'fact_map';
+  var parentID = 'fact_map';
   var size = 550;
   var radius = 5;
-  map.createHexmap(name, size, radius);
-
-  $('#colorbutton').click(function () {
-      map.updateHexmap(name);
-    });
+  this.camera = new map.Hexmap(parentID, size, radius);
+  $("#colorbutton").click(
+    $.proxy(function (){
+      var data =  d3.range(1440).map(d3.random.normal(2, 2));
+      this.camera.update(data, duration = 0);
+    },this)
+  );
 }
--- map.js
+++ map.js
@@ -1,16 +1,16 @@
 const d3 = require('d3');
 const d3_scale = require('d3-scale');
 
-
-function createCameraHexmap(element_id, size, radius) {
-  //get three variables, margin, width, height. width and height will be the dimensions of the svg container
+function Hexmap(parentID, size, radius) {
+  //Get three variables, margin, width, height. width and height will be the dimensions of the svg container
   //this svg will be square.
+  this.parentID = parentID;
 
-  var margin = {top: 20, right: 40, bottom: 20, left: 20},
+  var   margin = { top: 20, right: 40, bottom: 20, left: 20 },
           width = size - margin.left - margin.right,
           height = size - margin.top - margin.bottom;
 
-  var svg = d3.select('#' + element_id)
+  var svg = d3.select('#' + parentID)
             .append('svg')
             .attr('width', width)
             .attr('height', height)
@@ -18,8 +18,8 @@ function createCameraHexmap(element_id, size, radius) {
             .attr('id', 'hexmap');
 
   d3.csv('./pixel_positions.csv')
-      .row(function (csv_row) {
-          return csv_row;
+      .row(function (csvRow) {
+        return csvRow;
       })
       .get(function (error, rows) {
         var offsetX = width / 2.0;
@@ -28,48 +28,56 @@ function createCameraHexmap(element_id, size, radius) {
         var h = (Math.sqrt(3) / 2.0);
 
         var line = d3.svg.line()
-                  .x(function(d)  { return d.x;  })
-                  .y(function(d)  { return d.y;  })
+                  .x(function (d)  { return d.x;  })
+                  .y(function (d)  { return d.y;  })
                   .interpolate('linear');
 
-
-          var polys = svg.selectAll('path')
-                                .data(rows)
-                              .enter()
-                                .append('path')
-                                .attr('d', function (d, i){
-                                  var xp = d.pos_X*2*radius + offsetX;
-                                  var yp = d.pos_Y*2*radius + offsetY;
+        var polys = svg.selectAll('path')
+                              .data(rows)
+                            .enter()
+                              .append('path')
+                                .attr('d', function (d, i) {
+                                  var xp = d.pos_X * 2 * radius + offsetX;
+                                  var yp = d.pos_Y * 2 * radius + offsetY;
                                   hexagonData = [
-                                    { 'x': radius+xp,   'y': yp},
-                                    { 'x': radius/2+xp,  'y': radius*h+yp},
-                                    { 'x': -radius/2+xp,  'y': radius*h+yp},
-                                    { 'x': -radius+xp,  'y': yp},
-                                    { 'x': -radius/2+xp,  'y': -radius*h+yp},
-                                    { 'x': radius/2+xp, 'y': -radius*h+yp}
+                                    { x: radius + xp,   y: yp },
+                                    { x: radius / 2 + xp,  y: radius * h + yp },
+                                    { x: -radius / 2 + xp,  y: radius * h + yp },
+                                    { x: -radius + xp,  y: yp },
+                                    { x: -radius / 2 + xp,  y: -radius * h + yp },
+                                    { x: radius / 2 + xp, y: -radius * h + yp },
                                   ];
                                   return line(hexagonData);
                                 })
                                 .style('fill', function (d) {return '#858587';});
       });
+
+      this.update = function updateHexmap(data, duration = 150, scale=d3_scale.scaleInferno()) {
+        var min_data = d3.min(data);
+        var max_data = d3.max(data);
+
+        var color = scale.domain([min_data, max_data]);
+
+        svg = d3.select('#' + this.parentID + ' svg');
+        if (duration == 0){
+          var circles = svg.selectAll('path')
+                                .data(data)
+                                .style('fill', function (d) {
+                                  return color(d);
+                                });
+
+        } else {
+          var circles = svg.selectAll('path')
+                                .data(data)
+                                .transition()
+                                .duration(duration)
+                                .style('fill', function (d) {
+                                  return color(d);
+                                });
+        }
+      }
 }
 
-function updateHexmap(element_id, scale=d3_scale.scaleInferno(), duration = 150){
-    var data =  d3.range(1440).map(d3.random.normal(2,2));
 
-    var min_data = d3.min(data);
-    var max_data = d3.max(data);
 
-    var color = scale
-      .domain([min_data, max_data]);
-
-    svg = d3.select('#'+element_id + ' svg');
-    var circles = svg.selectAll('path')
-                          .data(data)
-                          .transition()
-                          .duration(duration)
-                          .style('fill', function (d) {
-                            return color(d);
-                          });
-}
-module.exports={createHexmap:createCameraHexmap, updateHexmap:updateHexmap };
+module.exports = {Hexmap: Hexmap };
