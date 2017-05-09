d2cc8f14c41b315c46e1c3534529f4782cf0f788
Kai B <kai.bruegge@tu-dortmund.de>
2017-05-09 10:36:10 +0200

    replace csv files with a js file

--- hexmap.js
+++ hexmap.js
@@ -3,10 +3,13 @@
 const d3 = require('d3');
 const d3_scale = require('d3-scale');
 
+const IDs = require('./pixel_ids.js')
+
 function Hexmap(parentID, size, radius, chid=true) {
   //Get three variables, margin, width, height. width and height will be the dimensions of the svg container
   //this svg will be square.
   this.parentID = parentID;
+  var ids = new IDs()
 
   var   margin = { top: 20, right: 20, bottom: 20, left: 20 },
           width = size - margin.left - margin.right,
@@ -19,47 +22,43 @@ function Hexmap(parentID, size, radius, chid=true) {
             .attr('class', 'hexmap')
             .attr('id', 'hexmap');
 
-  var path = './pixel_positions.csv'
+  var pixel_positions = ids.sorted_softid
   if(chid){
-    path = './pixel_positions_chid.csv'
+    pixel_positions = ids.sorted_chid
   }
-  d3.csv(path)
-      .row(function (csvRow) {
-        return csvRow;
-      })
-      .get(function (error, rows) {
-        var offsetX = width / 2.0;
-        var offsetY = height / 2.0;
 
-        var h = (Math.sqrt(3) / 2.0);
+  console.log(pixel_positions)
+  var offsetX = width / 2.0;
+  var offsetY = height / 2.0;
 
-        var line = d3.svg.line()
-                  .x(function (d)  { return d.x;  })
-                  .y(function (d)  { return d.y;  })
-                  .interpolate('linear');
+  var h = (Math.sqrt(3) / 2.0);
 
-        var polys = svg.selectAll('path')
-                              .data(rows)
-                            .enter()
-                              .append('path')
-                                .attr('d', function (d, i) {
-                                  var xp = d.pos_X * 2 * radius + offsetX;
-                                  var yp = d.pos_Y * 2 * radius + offsetY;
-                                  var hexagonData = [
-                                    { x: radius + xp,   y: yp },
-                                    { x: radius / 2 + xp,  y: radius * h + yp },
-                                    { x: -radius / 2 + xp,  y: radius * h + yp },
-                                    { x: -radius + xp,  y: yp },
-                                    { x: -radius / 2 + xp,  y: -radius * h + yp },
-                                    { x: radius / 2 + xp, y: -radius * h + yp },
-                                  ];
-                                  return line(hexagonData);
-                                })
-                                .style('fill', '#888888')
-                                .style('stroke', '#888888')
-                                .style('stroke-width', '1')
-                                .style('shape-rendering', 'geometricPrecision');
-      });
+  var line = d3.svg.line()
+            .x(function (d)  { return d.x;  })
+            .y(function (d)  { return d.y;  })
+            .interpolate('linear');
+
+  var polys = svg.selectAll('path')
+                        .data(pixel_positions)
+                      .enter()
+                        .append('path')
+                          .attr('d', function (d, i) {
+                            var xp = d.pos_X * 2 * radius + offsetX;
+                            var yp = d.pos_Y * 2 * radius + offsetY;
+                            var hexagonData = [
+                              { x: radius + xp,   y: yp },
+                              { x: radius / 2 + xp,  y: radius * h + yp },
+                              { x: -radius / 2 + xp,  y: radius * h + yp },
+                              { x: -radius + xp,  y: yp },
+                              { x: -radius / 2 + xp,  y: -radius * h + yp },
+                              { x: radius / 2 + xp, y: -radius * h + yp },
+                            ];
+                            return line(hexagonData);
+                          })
+                          .style('fill', '#888888')
+                          .style('stroke', '#888888')
+                          .style('stroke-width', '1')
+                          .style('shape-rendering', 'geometricPrecision');
 
   this.update = function updateHexmap(data, duration = 150, scale=d3_scale.scaleInferno()) {
     var min_data = d3.min(data);
